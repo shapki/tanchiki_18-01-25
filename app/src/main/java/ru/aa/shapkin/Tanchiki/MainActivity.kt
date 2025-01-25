298d04ba84ddaead557d4e1670a39ddec1028866
shapki <shapkin.igorek@gmail.com>
2025-01-25 14:38:47 +0300

    3 Presentation

--- app/src/main/java/ru/aa/shapkin/Tanchiki/MainActivity.kt
+++ app/src/main/java/ru/aa/shapkin/Tanchiki/MainActivity.kt
@@ -9,6 +9,8 @@ import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
 import android.view.KeyEvent.KEYCODE_DPAD_UP
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.FrameLayout
 import androidx.core.view.marginLeft
 import androidx.core.view.marginTop
@@ -23,6 +25,7 @@ const val CELL_SIZE = 50
 lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var editMode = false
     private val gridDrawer by lazy {
         GridDrawer(this)
     }
@@ -35,6 +38,17 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "Menu"
     }
 
+    private fun switchEditMode() {
+        if (editMode) {
+            gridDrawer.removeGrid()
+            binding.materialsContainer.visibility = INVISIBLE
+        } else {
+            gridDrawer.drawGrid()
+            binding.materialsContainer.visibility = VISIBLE
+        }
+        editMode = !editMode
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.settings, menu)
         return true
@@ -43,7 +57,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_settings -> {
-                gridDrawer.drawGrid()
+                switchEditMode()
                 return true
             }
 
